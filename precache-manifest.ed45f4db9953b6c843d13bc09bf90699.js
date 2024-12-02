96fa5abacf0d60e12196a25303fae652fb0b3863
Sid <sid@Sids-MacBook-Pro.local>
2024-12-02 11:09:25 -0500

    Updates

---
+++ precache-manifest.ed45f4db9953b6c843d13bc09bf90699.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56d183b8d5185d69541ddb996efe8af2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3a13af7ba0be2fe2139",
+    "url": "/static/css/2.da68fc70.chunk.css"
+  },
+  {
+    "revision": "ab69ef5a22233323c554",
+    "url": "/static/css/main.d4a8a4e2.chunk.css"
+  },
+  {
+    "revision": "e3a13af7ba0be2fe2139",
+    "url": "/static/js/2.1fec03be.chunk.js"
+  },
+  {
+    "revision": "17addde56dada538fe1bc33d341e4ae4",
+    "url": "/static/js/2.1fec03be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab69ef5a22233323c554",
+    "url": "/static/js/main.ea7fd12b.chunk.js"
+  },
+  {
+    "revision": "17d950002c762857ac58",
+    "url": "/static/js/runtime-main.7731aa82.js"
+  },
+  {
+    "revision": "ad9625c57c05e70d4c5b02bf63ce8072",
+    "url": "/static/media/5070701.ad9625c5.jpg"
+  },
+  {
+    "revision": "067e37630b8042cdffab83afb374aa05",
+    "url": "/static/media/Sid_Thakur.067e3763.jpg"
+  },
+  {
+    "revision": "4a90224c99166d94d4a0afc7fd62638f",
+    "url": "/static/media/idea-lab-flow.4a90224c.png"
+  },
+  {
+    "revision": "74854b8a5c552efc7ac87b559035c6ee",
+    "url": "/static/media/idea-launch-lab-map.74854b8a.png"
+  },
+  {
+    "revision": "5b31e7ca5433142821913f01e77001e6",
+    "url": "/static/media/intro_video_480p.5b31e7ca.mp4"
+  },
+  {
+    "revision": "cca4aaa43669a878037297e86fc5a04d",
+    "url": "/static/media/launch-crew.cca4aaa4.png"
+  },
+  {
+    "revision": "48086f588d679be72f0a06c31c0380de",
+    "url": "/static/media/product-lifecycle.48086f58.png"
+  }
+]);
